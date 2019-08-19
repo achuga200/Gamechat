1d376d92b767fda3c833767f3db5ff936f0c07ea
achuga2oo <achugamonyeokwunna@gmail.com>
2019-08-19 23:32:50 +0100

    signout implemented

--- app/src/main/java/com/example/gamechat/MainActivity.java
+++ app/src/main/java/com/example/gamechat/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.gamechat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -78,5 +84,33 @@ public class MainActivity extends AppCompatActivity {
             }
             }
         }
+
+        //To instantiate the menu resource inside MainActivity, override the onCreateOptionsMenu() method and call the inflate() method of the MenuInflater object
+
+    @Override
+    public  boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
+    //Next, override the onOptionsItemSelected() method to handle click events on the menu item. Inside the method, you can call the signOut() method of the AuthUI class to sign the user out. Because the sign-out operation is executed asynchronously, we'll also add an OnCompleteListener to it.
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        if(item.getItemId() == R.id.menu_sign_out) {
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>(){
+
+                        public void onComplete(@NonNull Task<Void> task){
+                            Toast.makeText(MainActivity.this,
+                                    "You have been signed out.",
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                            //close activity
+                            finish();
+                        }
+                    });
+        }
+        return true;
+    }
+}
 
